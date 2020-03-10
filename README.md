2c1cad94735d51650bc2d64929f641936cf11ac8
Adam Ahrens <adam.aahrens@gmail.com>
2020-03-09 21:24:31 -0500

    Adding remaining CRUD routes for Todo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advanced-web-json
 Advanced Web JSON API 
+
+# start
+`node index.js`
--- routes/todos.js
+++ routes/todos.js
@@ -7,6 +7,17 @@ router.get('/', function(request, response) {
     response.json({ api: 'v1' })
 })
 
+router.post('/todos', function(request, response) {
+    db.Todo.create(request.body).then(function(todo) {
+        console.log('--------------')
+        console.log('Created ' + todo)
+        console.log('--------------')
+        response.status(201).json(todo)
+    }).catch(function(error) {
+        response.send(error)
+    })
+})
+
 router.get('/todos', function(request, response) {
     db.Todo.find().then(function(todos){
         response.json(todos)
@@ -15,12 +26,25 @@ router.get('/todos', function(request, response) {
     })
 })
 
-router.post('/todos', function(request, response) {
-    db.Todo.create(request.body).then(function(todo) {
-        console.log('--------------')
-        console.log('Created ' + todo)
-        console.log('--------------')
-        response.status(201).json(todo)
+router.get('/todos/:todoId', function(request, response) {
+    db.Todo.findById(request.params.todoId).then(function(todo) {
+        response.json(todo)
+    }).catch(function(error) {
+        response.send(error)
+    })
+})
+
+router.put('/todos/:todoId', function(request, response) {
+    db.Todo.findOneAndUpdate({ _id: request.params.todoId }, request.body, { new: true }).then(function(todo) {
+        response.json(todo)
+    }).catch(function(error) {
+        response.send(error)
+    })
+})
+
+router.delete('/todos/:todoId', function(request, response) {
+    db.Todo.deleteOne({ _id: request.params.todoId }).then(function() {
+        response.json({result: 'deleted'})
     }).catch(function(error) {
         response.send(error)
     })
