67ec2401254bbdcf64b99e8352d7247fbf07b4db
Adam Ahrens <adam.aahrens@gmail.com>
2020-03-11 12:15:32 -0500

    Toggling todos comleted status

--- assets/app.js
+++ assets/app.js
@@ -15,6 +15,26 @@ $(document).ready(function () {
             $('#todoInput').val('');
         }
     })
+
+    // Listen for clicks on todos
+    $('.list').on('click', 'li', function () {
+        var todoId = $(this).attr('data-id');
+        var status = $(this).attr('data-complete')
+        var toggled = !$.parseJSON(status)
+        var path = '/api/todos/' + todoId
+        var li = $(this)
+
+        $.ajax({
+            url: path,
+            type: 'PUT',
+            data: { completed: toggled },
+            success: function (updatedTodo) {
+                // Do something with the result
+                $(li).toggleClass('done')
+                console.log(updatedTodo);
+            }
+        });
+    })
 });
 
 function createTodo(text) {
@@ -27,7 +47,8 @@ function addTodos(todos) {
     todos.forEach(element => {
         var todoText = element.name;
         var id = element._id
-        var li = $(`<li class="task" data-id="${id}">${todoText}</li>`)
+        var complete = element.completed
+        var li = $(`<li class="task" data-id="${id}" data-complete="${complete}">${todoText}</li>`)
 
         // Is it Done
         if (element.completed) {
