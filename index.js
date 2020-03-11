e1fcdb445622cbd2aae72c1efb1f7dd2d7d1912d
Adam Ahrens <adam.aahrens@gmail.com>
2020-03-11 09:35:14 -0500

    serving up static assets

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ var bodyParser = require('body-parser')
 app = express();
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/assets'))
 
 // Routes
 var todoRoutes = require('./routes/todos')
 app.use('/api', todoRoutes);
 
-
-app.get('/', function(request, response) {
-    response.send('Hello World')
-})
-
-app.get('/json', function(request, response) {
-    response.json({ hello: 'World', good: 'Evening', names: ['Leroy', 'Jenkins']})
+app.get('/', function (request, response) {
+    response.sendFile('index.html')
 })
 
 // Start
-app.listen(3001, function(){
+app.listen(3001, function () {
     console.log('express app running at http://localhost:3001')
 })
